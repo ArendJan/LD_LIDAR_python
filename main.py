73620948425c0d3b6c041aca81188b89a13874a9
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2023-09-21 11:54:36 +0200

    Fix faster serial parsing

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import serial
-import binascii
 from CalcLidarData import CalcLidarData
 import matplotlib.pyplot as plt
 import math
-
 fig = plt.figure(figsize=(8,8))
 ax = fig.add_subplot(111, projection='polar')
 ax.set_title('lidar (exit: Key E)',fontsize=18)
 
-# Eキーを押すと終了します。
 plt.connect('key_press_event', lambda event: exit(1) if event.key == 'e' else None)
 
-ser = serial.Serial(port='/dev/tty.usbserial-0001',
+ser = serial.Serial(port='COM69',
                     baudrate=230400,
                     timeout=5.0,
                     bytesize=8,
@@ -19,31 +16,24 @@ ser = serial.Serial(port='/dev/tty.usbserial-0001',
                     stopbits=1)
 
 tmpString = ""
-lines = list()
 angles = list()
 distances = list()
+prevLine = None
 
-i = 0
+def the_callback(angles, distances):
+    global prevLine
+    if not prevLine is None:
+        prevLine.remove()
+    line = ax.scatter([-angle for angle in angles], distances, c="pink", s=5)
+    ax.set_theta_offset(math.pi / 2)
+    plt.pause(0.01)
+    prevLine = line
+
 while True:
-    loopFlag = True
-    flag2c = False
-
-    if(i % 40 == 39):
-        if('line' in locals()):
-            line.remove()
-        line = ax.scatter(angles, distances, c="pink", s=5)
-
-        ax.set_theta_offset(math.pi / 2)
-        plt.pause(0.01)
-        angles.clear()
-        distances.clear()
-        i = 0
-        
-
-    while loopFlag:
-        b = ser.read()
-        tmpInt = int.from_bytes(b, 'big')
-        
+    all_b = ser.read_all()
+    for b in all_b:
+        tmpInt = int( b)
+        b = bytes([b])
         if (tmpInt == 0x54):
             tmpString +=  b.hex()+" "
             flag2c = True
@@ -51,7 +41,6 @@ while True:
         
         elif(tmpInt == 0x2c and flag2c):
             tmpString += b.hex()
-
             if(not len(tmpString[0:-5].replace(' ','')) == 90 ):
                 tmpString = ""
                 loopFlag = False
@@ -61,14 +50,17 @@ while True:
             lidarData = CalcLidarData(tmpString[0:-5])
             angles.extend(lidarData.Angle_i)
             distances.extend(lidarData.Distance_i)
-                
+            if(len(angles) > 50*12):
+                split = [i for i in range(len(angles)-1) if angles[i+1]< angles[i]]
+                first = angles[:split[0]+1]
+                angles = angles[split[0]+1:]
+                firstDist = distances[:split[0]+1]
+                distances = distances[split[0]+1:]
+                the_callback(first, firstDist)
             tmpString = ""
-            loopFlag = False
         else:
             tmpString += b.hex()+" "
         
         flag2c = False
-    
-    i +=1
 
 ser.close()
